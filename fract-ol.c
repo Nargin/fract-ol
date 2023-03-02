4d5dd2186b7f9166288dd24b5cd6b7d02d162bf0
Nrgn <mejumine@gmail.com>
2023-03-02 18:18:35 +0100

    carre youhou

--- fract-ol.c
+++ fract-ol.c
@@ -6,7 +6,7 @@
 /*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 21:59:02 by romaurel          #+#    #+#             */
-/*   Updated: 2023/02/22 18:02:42 by romaurel         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:15:04 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,20 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 int	main(void)
 {
 	void	*mlx;
-	int	i = 4;
+	void	*mlx_win;
+	int	i;
+	int	j = 4;
 	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Fract-ol.jpg");
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	while (++i < 200)
-		my_mlx_pixel_put(&img, 5, i, 0x00FFFFFF);
+	while (++j < 500){
+		i = 4;
+		while (++i < 500)
+			my_mlx_pixel_put(&img, j, i, 0xFFFF00);
+	}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
