99e8bccdc33836ae532d145c587eebb297130cda
Nargin <mejumine@gmail.com>
2023-03-03 15:24:18 +0100

    struct

--- fract-ol.h
+++ fract-ol.h
@@ -6,7 +6,7 @@
 /*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 22:03:05 by romaurel          #+#    #+#             */
-/*   Updated: 2023/02/22 17:58:03 by romaurel         ###   ########.fr       */
+/*   Updated: 2023/03/03 15:20:11 by robin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,30 @@
 # include <string.h>
 # include <math.h>
 # include <mlx.h>
+
+typedef struct	s_variable {
+	double	h;
+	double	w;
+	double	cRe;
+	double	cIm;
+	double	newRe;
+	double	newIm;
+	double	oldRe;
+	double	oldIm;
+	double	zoom;
+	double	moveX;
+	double	moveY;
+}				t_var;
+
+typedef struct	s_window {
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	void	*addr;
+	int		bits_per_pixel;
+	int		line_lenght;
+	int		endian;
+	t_var	c;
+}				t_win
 
 #endif
